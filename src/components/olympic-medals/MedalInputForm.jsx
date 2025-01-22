533b30f28979958a5d26855877c6390d943f83dd
노수민 <nosumin29@gmail.com>
2025-01-22 15:52:37 +0900

    refactor: Form Area 컴포넌트 분리
    - Button Component
    - Input Component
    - InputForm Component

--- src/components/olympic-medals/MedalInputForm.jsx
+++ src/components/olympic-medals/MedalInputForm.jsx
@@ -1,7 +1,22 @@
-const MedalInputForm = () => {
-  return (
-    <div>MedalInputForm</div>
-  )
-}
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
 
-export default MedalInputForm
+const Form = styled.form`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  gap: 16px;
+`
+const MedalInputForm = ({handleSubmit, children}) => {
+  return (
+    <Form onSubmit={handleSubmit}>
+      {children}
+    </Form>
+  );
+};
+
+MedalInputForm.propTypes = {
+  handleSubmit: PropTypes.func.isRequired,
+  children: PropTypes.node.isRequired,
+};
+
+export default MedalInputForm;
--- src/layouts/Content.jsx
+++ src/layouts/Content.jsx
@@ -1,30 +1,35 @@
-import { useState } from 'react'
-import styles from './Content.module.css'
+import { useState } from 'react';
+import styles from './Content.module.css';
 import { getLocalStorage, setLocalStorage } from '../storage/localStorage';
+import MedalInputForm from '../components/olympic-medals/MedalInputForm';
+import Input from '../components/common/Input';
+import Button from '../components/common/Button';
 
 const Content = () => {
   const [nationList, setNationList] = useState(() => {
-    return getLocalStorage('nationList') || []
+    return getLocalStorage('nationList') || [];
   });
-  
+
   const [nation, setNation] = useState({
     id: crypto.randomUUID(),
     nationName: '',
     goldMedalCnt: 0,
     silverMedalCnt: 0,
-    bronzeMedalCnt: 0
-  })
+    bronzeMedalCnt: 0,
+  });
 
   const nationInputHandler = (e) => {
-    const {value, name} = e.target;
-    setNation(pre => ({
+    const { value, name } = e.target;
+    setNation((pre) => ({
       ...pre,
-      [name]: name === 'goldMedalCnt' 
-        || name === 'silverMedalCnt' 
-        || name === 'bronzeMedalCnt' 
-        ? Number(value) : value
-    }))
-  }
+      [name]:
+        name === 'goldMedalCnt' ||
+        name === 'silverMedalCnt' ||
+        name === 'bronzeMedalCnt'
+          ? Number(value)
+          : value,
+    }));
+  };
 
   const init = () => {
     setNation({
@@ -32,138 +37,150 @@ const Content = () => {
       nationName: '',
       goldMedalCnt: 0,
       silverMedalCnt: 0,
-      bronzeMedalCnt: 0
-    })
-  }
+      bronzeMedalCnt: 0,
+    });
+  };
 
   const addNationHandler = (e) => {
-    e.preventDefault()
-    if(!nation.nationName){
-      return alert('국가명을 입력하세요.')
+    e.preventDefault();
+    if (!nation.nationName) {
+      return alert('국가명을 입력하세요.');
     }
-    if(nationList.filter(e => e.nationName === nation.nationName).length > 0){
+    if (
+      nationList.filter((e) => e.nationName === nation.nationName).length > 0
+    ) {
       init();
       return alert('이미 등록된 국가입니다.');
     }
-    const newNationList = [...nationList, nation]
-    setNationList(newNationList)
-    setLocalStorage('nationList', newNationList)
+    const newNationList = [...nationList, nation];
+    setNationList(newNationList);
+    setLocalStorage('nationList', newNationList);
     init();
-  }
+  };
 
   const modifyNationHandler = (e) => {
     e.preventDefault();
-    
+
     const updatedList = nationList.map((pre) => {
-      if(pre.nationName === nation.nationName){
-        return {...pre, goldMedalCnt: nation.goldMedalCnt, silverMedalCnt: nation.silverMedalCnt, bronzeMedalCnt: nation.bronzeMedalCnt}
+      if (pre.nationName === nation.nationName) {
+        return {
+          ...pre,
+          goldMedalCnt: nation.goldMedalCnt,
+          silverMedalCnt: nation.silverMedalCnt,
+          bronzeMedalCnt: nation.bronzeMedalCnt,
+        };
       }
       return pre;
-    })
-    
+    });
 
-    setNationList(updatedList)
+    setNationList(updatedList);
     setLocalStorage('nationList', updatedList);
     init();
-  }
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
     // 어떤 버튼이 눌렸는지 확인
     const clickedButton = e.nativeEvent.submitter.name;
-    if(clickedButton === 'add'){
-      addNationHandler(e)
+    if (clickedButton === 'add') {
+      addNationHandler(e);
     } else {
       modifyNationHandler(e);
     }
-  }
+  };
 
   const deleteNationHandler = (id) => {
-    if(confirm('해당 국가를 삭제하시겠습니까?')){
-      const newList = nationList.filter(e => e.id !== id)
+    if (confirm('해당 국가를 삭제하시겠습니까?')) {
+      const newList = nationList.filter((e) => e.id !== id);
       setNationList(newList);
       setLocalStorage('nationList', newList);
       init();
       return alert('삭제되었습니다!');
     }
-  }
+  };
 
-  const {nationName, goldMedalCnt, silverMedalCnt, bronzeMedalCnt} = nation
+  const { nationName, goldMedalCnt, silverMedalCnt, bronzeMedalCnt } = nation;
   return (
     <div className={styles.content}>
-    <form className={styles.formGrid} onSubmit={handleSubmit}>
-      <input 
-        type="text" 
-        name="nationName" 
-        value={nationName} 
-        onChange={nationInputHandler} 
-        placeholder='국가명'
-        className={[styles.inputFormat, styles.inputMain].join(' ')}
-      />
-      <input
-        type="number"
-        name="goldMedalCnt"
-        value={goldMedalCnt}
-        onChange={nationInputHandler}
-        placeholder='금메달'
-        className={[styles.inputFormat, styles.inputSub].join(' ')}
-      />
-      <input
-        type="number"
-        name="silverMedalCnt"
-        value={silverMedalCnt}
-        onChange={nationInputHandler}
-        placeholder='은메달'
-        className={[styles.inputFormat, styles.inputSub].join(' ')}
-      />
-      <input
-        type="number"
-        name="bronzeMedalCnt"
-        value={bronzeMedalCnt}
-        onChange={nationInputHandler}
-        placeholder='동메달'
-        className={[styles.inputFormat, styles.inputSub].join(' ')}
-      />
-      <button 
-        type="submit"
-        name="add"
-        className={styles.formButton}
-        >
-        {/* onClick={(e) => addNationHandler(e)} */}
-        국가 추가
-      </button>
-      <button 
-        type="submit"
-        name="update"
-        className={styles.formButton}
-        >
-        {/* onClick={(e) => modifyNationHandler(e)} */}
-        업데이트
-      </button>
-    </form>
-    {nationList && nationList.length > 0 ? (
-      <table className={styles.tableWrapper}>
-        <thead className={styles.tableHeader}>
-          <tr>
-            <th>국가</th>
-            <th>금메달</th>
-            <th>은메달</th>
-            <th>동메달</th>
-            <th>총메달</th>
-            <th>액션</th>
-          </tr>
-        </thead>
-        <tbody>
-          {
-            nationList.map((nation) => (
+      <MedalInputForm
+        handleSubmit={handleSubmit}
+        nationName={nationName}
+        nationInputHandler={nationInputHandler}
+        goldMedalCnt={goldMedalCnt}
+        silverMedalCnt={silverMedalCnt}
+        bronzeMedalCnt={bronzeMedalCnt}
+      >
+        <Input
+          type="text"
+          name="nationName"
+          value={nationName}
+          onChange={nationInputHandler}
+          placeholder="국가명"
+          className={[styles.inputFormat, styles.inputMain].join(' ')}
+        />
+        <Input
+          type="number"
+          name="goldMedalCnt"
+          value={goldMedalCnt.toString()}
+          onChange={nationInputHandler}
+          placeholder="금메달"
+          className={[styles.inputFormat, styles.inputSub].join(' ')}
+        />
+        <Input
+          type="number"
+          name="silverMedalCnt"
+          value={silverMedalCnt.toString()}
+          onChange={nationInputHandler}
+          placeholder="은메달"
+          className={[styles.inputFormat, styles.inputSub].join(' ')}
+        />
+        <Input
+          type="number"
+          name="bronzeMedalCnt"
+          value={bronzeMedalCnt.toString()}
+          onChange={nationInputHandler}
+          placeholder="동메달"
+          className={[styles.inputFormat, styles.inputSub].join(' ')}
+        />
+        <Button 
+          type="submit" 
+          name="add" 
+          className={styles.formButton}>
+          국가 추가
+        </Button>
+        <Button 
+          type="submit" 
+          name="update" 
+          className={styles.formButton}>
+          업데이트
+        </Button>
+      </MedalInputForm>
+      {nationList && nationList.length > 0 ? (
+        <table className={styles.tableWrapper}>
+          <thead className={styles.tableHeader}>
+            <tr>
+              <th>국가</th>
+              <th>금메달</th>
+              <th>은메달</th>
+              <th>동메달</th>
+              <th>총메달</th>
+              <th>액션</th>
+            </tr>
+          </thead>
+          <tbody>
+            {nationList.map((nation) => (
               <tr key={nation.id} className={styles.tableRow}>
                 <td>{nation.nationName}</td>
                 <td className={styles.goldMedal}>{nation.goldMedalCnt}</td>
                 <td className={styles.silverMedal}>{nation.silverMedalCnt}</td>
                 <td className={styles.bronzeMedal}>{nation.bronzeMedalCnt}</td>
-                <td>{nation.goldMedalCnt + nation.silverMedalCnt + nation.bronzeMedalCnt}</td>
                 <td>
-                  <button 
+                  {nation.goldMedalCnt +
+                    nation.silverMedalCnt +
+                    nation.bronzeMedalCnt}
+                </td>
+                <td>
+                  <button
                     className={styles.actionButton}
                     onClick={() => deleteNationHandler(nation.id)}
                   >
@@ -171,14 +188,16 @@ const Content = () => {
                   </button>
                 </td>
               </tr>
-            ))
-          }
-        </tbody>
-      </table>)
-      : <span className={styles.noData}>아직 추가된 국가가 없습니다. 메달을 추적하세요!</span>
-    }
+            ))}
+          </tbody>
+        </table>
+      ) : (
+        <span className={styles.noData}>
+          아직 추가된 국가가 없습니다. 메달을 추적하세요!
+        </span>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Content
+export default Content;
