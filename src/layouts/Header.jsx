caa1a5aa80318bdf7c2a39f4c6637a4e842e15f7
노수민 <nosumin29@gmail.com>
2025-01-23 12:22:26 +0900

    fix: header text 변경

--- src/layouts/Header.jsx
+++ src/layouts/Header.jsx
@@ -3,7 +3,7 @@ import styles from './Header.module.css';
 const Header = () => {
   return (
     <div className={styles.header}>
-      <h1>2024 파리 올림픽</h1>
+      <h1>2024 파리 올림픽 매달 순위 관리표</h1>
     </div>
   );
 };
