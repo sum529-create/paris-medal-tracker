46cb0148f71dd5b2f5163ffe09ed0512336dd15c
노수민 <nosumin29@gmail.com>
2025-01-24 19:39:06 +0900

    chore: run prettier on entire project

--- src/components/olympic-medals/MedalInputForm.jsx
+++ src/components/olympic-medals/MedalInputForm.jsx
@@ -6,25 +6,21 @@ const Form = styled.form`
   grid-template-columns: repeat(4, 1fr);
   gap: 16px;
   @media (max-width: 768px) {
-   grid-template-columns: repeat(6, 1fr);
-   gap: 8px;
- }
-`
+    grid-template-columns: repeat(6, 1fr);
+    gap: 8px;
+  }
+`;
 
 /**
  * MedalInputForm 컴포넌트
  * - 폼 제출 핸들러와 자식 요소를 받아 폼을 구성하는 컴포넌트이다.
- * 
+ *
  * @param {object} param0 - 컴포넌트에 전달되는 props 객체
  * @param {function} param0.handleSubmit - 폼 제출 시 호출되는 함수
  * @param {React.ReactNode} param0.children - 폼 내에 렌더링할 자식 요소들
  */
-const MedalInputForm = ({handleSubmit, children}) => {
-  return (
-    <Form onSubmit={handleSubmit}>
-      {children}
-    </Form>
-  );
+const MedalInputForm = ({ handleSubmit, children }) => {
+  return <Form onSubmit={handleSubmit}>{children}</Form>;
 };
 
 MedalInputForm.propTypes = {
--- src/components/olympic-medals/MedalTableRow.jsx
+++ src/components/olympic-medals/MedalTableRow.jsx
@@ -1,36 +1,38 @@
-import '../../assets/styles/table.css'
+import '../../assets/styles/table.css';
 import PropTypes from 'prop-types';
 
 /**
  * MedalTableRow 컴포넌트
  * - 매달 데이터를 표시하는 테이블을 렌더링
- * 
- * @param {node} children - table 마지막 요소에 포함될 데이터 (액션 버튼) 
+ *
+ * @param {node} children - table 마지막 요소에 포함될 데이터 (액션 버튼)
  * @param {object} value - table 데이터에 표시될 데이터
  */
-const MedalInputFormRow = ({children, value}) => {
-  const {nationName, goldMedalCnt, silverMedalCnt, bronzeMedalCnt} = value
+const MedalInputFormRow = ({ children, value }) => {
+  const { nationName, goldMedalCnt, silverMedalCnt, bronzeMedalCnt } = value;
   return (
     <tr className="table-row">
       <td data-label="국가">{nationName}</td>
-      <td data-label="금메달" className="gold-medal">{goldMedalCnt}</td>
-      <td data-label="은메달" className="silver-medal">{silverMedalCnt}</td>
-      <td data-label="동메달" className="bronze-medal">{bronzeMedalCnt}</td>
+      <td data-label="금메달" className="gold-medal">
+        {goldMedalCnt}
+      </td>
+      <td data-label="은메달" className="silver-medal">
+        {silverMedalCnt}
+      </td>
+      <td data-label="동메달" className="bronze-medal">
+        {bronzeMedalCnt}
+      </td>
       <td data-label="총메달">
-        {goldMedalCnt +
-          silverMedalCnt +
-          bronzeMedalCnt}
+        {goldMedalCnt + silverMedalCnt + bronzeMedalCnt}
       </td>
-      <td>
-        {children}
-      </td>
+      <td>{children}</td>
     </tr>
-  )
-}
+  );
+};
 
 MedalInputFormRow.propTypes = {
   children: PropTypes.node.isRequired,
-  value: PropTypes.object.isRequired
-}
+  value: PropTypes.object.isRequired,
+};
 
-export default MedalInputFormRow
+export default MedalInputFormRow;
